e3f9b14504fd4b5adf4501dac4f689d48ce5c7ca
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2021-08-19 08:47:50 +0300

    added remove method

--- src/base.service.ts
+++ src/base.service.ts
@@ -7,4 +7,11 @@ export const BaseServiceImpl: BaseService = Object.freeze({
   addTodo(todo: string): void {
     this.list$.next([...this.list$.getValue(), todo]);
   },
+  removeTodo(todoIndex: number): void {
+    const updatedList = this.list$
+      .getValue()
+      .filter((el, index) => index !== todoIndex);
+
+    this.list$.next(updatedList);
+  },
 });
