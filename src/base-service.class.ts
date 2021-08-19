51cffe1d2040e60c8ac108c1d6c715e4e923a61d
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2021-08-19 08:49:19 +0300

    updated interface

--- src/base-service.class.ts
+++ src/base-service.class.ts
@@ -3,4 +3,5 @@ import { BehaviorSubject } from "rxjs";
 export abstract class BaseService {
   abstract list$: BehaviorSubject<string[]>;
   abstract addTodo(todo: string): void;
+  abstract removeTodo(index: number): void;
 }
